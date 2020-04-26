9affeab7ef0c46710fe535c0227610b4c0700399
Benjamin Abraham <benjaminab1184@gmail.com>
2020-04-26 08:58:23 +0530

    adding header component and links to home, order page and contact

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
-import OrderPage from './pages/order/order.component';
-
-
+import OrderPage from "./pages/order/order.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/order" component={OrderPage} />
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/kdk.svg";
+
+import "./header.styles.scss";
+
+const Header = () => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/order">
+        ORDER
+      </Link>
+      <Link className="option" to="/order">
+        CONTACT
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
