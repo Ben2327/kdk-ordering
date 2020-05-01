e0998ec0d81b76b4f7f5cd0a59ed6705500117e4
Benjamin Abraham <benjaminab1184@gmail.com>
2020-05-01 17:37:46 +0530

    updated order data with new data, updated cart utils to group items in cart reducer

--- src/components/order-item/order-item.component.jsx
+++ src/components/order-item/order-item.component.jsx
@@ -1,20 +1,33 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
 
 import "./order-item.styles.scss";
 
-const OrderItem = ({ id, name, price, imageUrl }) => (
-  <div className="order-item">
-    <div className="image" 
-    style  =
-    {{
-      backgroundImage: `url(${imageUrl})`,
-    }}
-    />
-    <div className="order-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const OrderItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className="order-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="order-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
 
-export default OrderItem;
+export default connect(null, mapDispatchToProps)(OrderItem);
--- src/components/order-preview/order-preview.component.jsx
+++ src/components/order-preview/order-preview.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./order-preview.styles.scss";
-import Orderitem from '../order-item/order-item.component';
+import Orderitem from "../order-item/order-item.component";
 
 const OrderPreview = ({ title, items }) => (
   <div className="order-preview">
@@ -9,8 +9,8 @@ const OrderPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(({id, ...OtherItemProps}) => (
-          <Orderitem key={id}{...OtherItemProps}/>
+        .map((item) => (
+          <Orderitem key={item.id} item={item} />
         ))}
     </div>
   </div>
